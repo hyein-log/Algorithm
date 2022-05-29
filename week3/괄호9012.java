df6ed0fb4f4e0ac4caa04e75d9f3eb287e46af59
Yujin Hong <hhhyj1077@naver.com>
2022-05-29 22:38:41 +0900

    +괄호

--- week3/괄호9012.java
+++ week3/괄호9012.java
@@ -1,10 +1,55 @@
 package week3;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
 public class °ýÈ£9012 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		for(int i = 0; i<n; i++) {
+			String[] arr = br.readLine().split("");
+			ArrayList<Integer> aList = new ArrayList<>();
+			for(int j = 0; j<arr.length; j++) {
+				if(arr[j].equals("(")) {
+					aList.add(0);
+				}else {
+					aList.add(1);
+				}
+			}
+			String result = checkArr(aList);
+			System.out.println(result);
+		}
+	}
 
+	private static String checkArr(ArrayList<Integer> aList) {
+		if(aList.size()==0) {
+			return "YES";
+		}else {
+			for(int i = 0; i<aList.size(); i++) {
+				if(aList.get(i)==1) {
+					if(i==0) {
+						return "NO";
+					}
+					else if(aList.get(i-1)==0) {
+						aList.remove(i);
+						aList.remove(i-1);
+						break;
+					}else {
+						return "NO";
+					}
+					
+				}else if(i==aList.size()-1) {
+					return "NO";
+				}
+			}
+			return checkArr(aList);
+		}
+		
 	}
 
 }
---
+++ week3/단어뒤집기9093.java
@@ -0,0 +1,35 @@
+package week3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 단어뒤집기9093 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int n = Integer.parseInt(br.readLine());
+		String[][] arr = new String[n][];
+		for(int i = 0; i<n; i++) {
+			arr[i]= br.readLine().split(" ");
+		}
+		for(int i = 0; i<arr.length; i++) {
+			for(int j = 0; j<arr[i].length; j++) {
+				if(arr[i][j].length()==1) {
+					sb.append(arr[i][j]);
+					sb.append(" ");
+				}else {
+					for(int k = arr[i][j].length()-1;k>=0;k--) {
+						sb.append(arr[i][j].charAt(k));
+					}
+					sb.append(" ");
+				}
+			}
+			sb.append("\n");
+		}
+		System.out.println(sb);
+	}
+
+}
